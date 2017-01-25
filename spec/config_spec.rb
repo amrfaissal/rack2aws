3dc45cdba456b76324b3e7376249044d9d4a4d14
Faissal Elamraoui <amr.faissal@gmail.com>
2017-01-25 22:32:26 +0100

    add more tests in config_spec

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,36 +2,71 @@ require 'rack2aws/config'
 
 include Rack2Aws::Configuration
 
-describe RackspaceConfig do
-  describe ".load" do
-    before {
-      RackspaceConfig.instance_variable_set(:@config_path, "#{Dir.getwd}/spec/config")
-    }
+describe KVConfigParser do
+  let (:credentials) { KVConfigParser.new("#{Dir.getwd}/spec/credentials") }
+  it 'parses a key-value configuration file' do
+    expect(credentials.get_params).to eq(["default", "others"])
+    expect(credentials.get_groups).to eq(["default", "others"])
+    expect(credentials['default']).to eq({"region"=>"eu-west-1","aws_access_key_id"=>"dummy_access_key_id", "aws_secret_access_key"=>"dummy_secret_access_key"})
+  end
+end
 
-    it "returns Rackspace configuration hash" do
-      expect(RackspaceConfig.load()).to eql({
-                                              :provider => 'Rackspace',
-                                              :rackspace_api_key => 'your_api_key',
-                                              :rackspace_username => 'your_username',
-                                              :rackspace_region => 'ORD'
-                                            })
+describe RackspaceConfig do
+  describe '.load' do
+    context 'given a non-existant configuration file' do
+      before {
+        RackspaceConfig.instance_variable_set(:@config_path, '/path/to/non-existant-file')
+      }
+
+      it 'raises a FileNotFoundError exception' do
+        expect { RackspaceConfig.load }.to raise_error(FileNotFoundError)
+      end
+    end
+
+    context 'given a valid configuration file' do
+      before {
+        RackspaceConfig.instance_variable_set(:@config_path, "#{Dir.getwd}/spec/config")
+      }
+
+      it 'returns Rackspace configuration hash' do
+        expected_result = {
+          :provider => 'Rackspace',
+          :rackspace_api_key => 'your_api_key',
+          :rackspace_username => 'your_username',
+          :rackspace_region => 'ORD'
+        }
+        expect(RackspaceConfig.load()).to eql(expected_result)
+      end
     end
   end
 end
 
 describe AWSConfig do
-  describe ".load" do
-    before {
-      AWSConfig.instance_variable_set(:@config_path, File.expand_path('spec/credentials'))
-    }
+  describe '.load' do
+    context 'given a non-existant configuration file' do
+      before {
+        AWSConfig.instance_variable_set(:@config_path, '/path/to/non-existant-file')
+      }
 
-    it "returns AWS configuration hash" do
-      expect(AWSConfig.load()).to eql({
-                                        :provider => 'AWS',
-                                        :region => 'eu-west-1',
-                                        :aws_access_key_id => 'dummy_access_key_id',
-                                        :aws_secret_access_key => 'dummy_secret_access_key'
-                                      })
+      it 'raises a FileNotFoundError exception' do
+        expect { AWSConfig.load }.to raise_error(FileNotFoundError)
+      end
+    end
+
+    context 'given a valid configuration file' do
+      before {
+        AWSConfig.instance_variable_set(:@config_path, File.expand_path('spec/credentials'))
+      }
+
+      it 'returns AWS configuration hash' do
+        expected_result = {
+          :provider => 'AWS',
+          :region => 'eu-west-1',
+          :aws_access_key_id => 'dummy_access_key_id',
+          :aws_secret_access_key => 'dummy_secret_access_key'
+        }
+        expect(AWSConfig.load).to eql(expected_result)
+      end
     end
   end
 end
